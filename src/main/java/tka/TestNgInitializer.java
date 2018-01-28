859d670d7cbf110008ed94e980634eaf8d4ded33
otkagit <aat91070@googlemail.com>
2018-01-28 19:07:48 +0100

    sys props

--- src/main/java/tka/TestNgInitializer.java
+++ src/main/java/tka/TestNgInitializer.java
@@ -2,6 +2,7 @@ package tka;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -13,7 +14,8 @@ public class TestNgInitializer {
 
 	@BeforeSuite
 	public void beforeSuite() {
-		System.setProperty("webdriver.chrome.driver", "/Users/Olena/Documents/Projekte/JAVA/chromedriver");
+//		System.setProperty("webdriver.chrome.driver", "/Users/Olena/Documents/Projekte/JAVA/chromedriver");
+//		System.setProperty("webdriver.gecko.driver", "/Users/Olena/Documents/Projekte/JAVA/geckodriver");
 	}
 
 	@AfterSuite
@@ -22,7 +24,28 @@ public class TestNgInitializer {
 
 	@BeforeMethod
 	public void beforeMethod() {
-		driver = new ChromeDriver();
+		
+		String br = System.getProperty("browser");
+		String crDr = System.getProperty("webdriver.chrome.driver");
+		String ffDr = System.getProperty("webdriver.gecko.driver");
+
+		System.out.println("SYSTEM-PROP BROWSER=" + br);
+		System.out.println("SYSTEM-PROP CH_DR=" + crDr);
+		System.out.println("SYSTEM-PROP FF_DR=" + ffDr);
+
+		
+		switch (br) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+			
+		default:
+			throw new RuntimeException("Browser not set");
+		}
 	}
 
 	@AfterMethod
